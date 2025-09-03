a56ce202ed35a273cbb266281435ee79f76b205f
W.llama <106076670+W-llama@users.noreply.github.com>
2025-09-03 22:13:08 +0900

    init commit

---
+++ src/test/java/com/example/jpaoptimizationlab/JpaOptimizationLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaoptimizationlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaOptimizationLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
